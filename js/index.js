40af0e83d49232edb3d34eb84ab9f061ab23e82f
Oleksandr Kropivnyi <kropivnyy@gmail.com>
2025-08-02 23:48:17 +0300

    test percent

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ const showContent = () => {
 
 const setSkewPercent = () => {
   const ratio = window.innerHeight / window.innerWidth;
-  const percent = -30.97 * ratio - 3.3;
+  const percent = -30.97 * ratio - 2.6;
   REFS.root.style.setProperty("--skewed-shift", `${percent}%`);
 };
 
